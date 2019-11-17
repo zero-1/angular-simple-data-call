7a1bd5d86f9e5055cc04d1a047a2ac44c2c87fba
zero-1 <arjun.shankar@outlook.com>
2019-11-18 05:55:04 +1100

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TableComponent } from './component/table/table.component';
 import { DataService } from './service/data.service';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, TableComponent ],
+  declarations: [ AppComponent, TableComponent ],
   bootstrap:    [ AppComponent ],
   providers: [DataService]
 })
